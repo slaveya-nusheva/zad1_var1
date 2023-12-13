cec18d5b48777e5c4e40adc579ac9cd0e0b4ff9f
s.nusheva <slaveyanusheva@gmail.com>
2023-12-13 15:26:01 +0200

    constructor in vehicle

--- src/com/company/Vehicle.java
+++ src/com/company/Vehicle.java
@@ -3,5 +3,13 @@ package com.company;
 public abstract class Vehicle {
     boolean isActive;
 
+    Vehicle() {
+        this.isActive = true;
+    }
+
+    Vehicle(boolean isActive) {
+        this.isActive = isActive;
+    }
+
     public abstract void drive();
 }
